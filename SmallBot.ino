531605fcfa8bfab5361bd8dcadf3f7f4eef093f1
Ajen8005 <45045045+Ajen8005@users.noreply.github.com>
2020-01-19 20:32:25 -0500

    Update SmallBot.ino

--- SmallBot.ino
+++ SmallBot.ino
@@ -10,27 +10,24 @@
 #include <Wire.h>
 #include <Adafruit_PWMServoDriver.h>
 
-// called this way, it uses the default address 0x40
 Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
-// you can also call it with a different address you want
-//Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver(0x41);
-// you can also call it with a different address and I2C interface
-//Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver(0x40, Wire);
 
 // Depending on your servo make, the pulse width min and max may vary, you 
 // want these to be as small/large as possible without hitting the hard stop
 // for max range. You'll have to tweak them as necessary to match the servos you
 // have!
-#define SERVOMIN  150 // This is the 'minimum' pulse length count (out of 4096)
-#define SERVOMAX  600 // This is the 'maximum' pulse length count (out of 4096)
-#define USMIN  600 // This is the rounded 'minimum' microsecond length based on the minimum pulse of 150
-#define USMAX  2400 // This is the rounded 'maximum' microsecond length based on the maximum pulse of 600
+//350 - 180 degree
+//SERVOMIN 50 is 45 degrees
+// Every 50 is 22.5 degrees for servo max when servo min is 50
+// Every 5 added to SERVOMAX adds 2.25 degrees
+#define SERVOMIN  200// This is the 'minimum' pulse length count (out of 4096)
+#define SERVOMAX  300// This is the 'maximum' pulse length count (out of 4096)
 #define SERVO_FREQ 50 // Analog servos run at ~50 Hz updates
 
 // our servo # counter
-uint8_t servonum[2769] = {3,1,4,1,5,9,2,6,5,3,5,8,9,7,9,3,2,3,8,4,6,2,6,4,3,3,8,3};
+uint8_t servonum[100] = {3,1,4,1,5,9,2,6,5,3,5,8,9,7,9,3,2,3,8,4,6,2,6,4,3,3,8,3};
 uint8_t Counter = 0;
-uint8_t FrontBumper, BackBumper;
+uint8_t FrontBumper, BackBumper, BackFlg = 0, Front = 0, startFlg = 0;
 
 void setup() {
   Serial.begin(9600);
@@ -46,41 +43,40 @@ void setup() {
   delay(10);
 }
 
-// You can use this function if you'd like to set the pulse length in seconds
-// e.g. setServoPulse(0, 0.001) is a ~1 millisecond pulse width. It's not precise!
-void setServoPulse(uint8_t n, double pulse) {
-  double pulselength;
-  
-  pulselength = 1000000;   // 1,000,000 us per second
-  pulselength /= SERVO_FREQ;   // Analog servos run at ~60 Hz updates
-  Serial.print(pulselength); Serial.println(" us per period"); 
-  pulselength /= 4096;  // 12 bits of resolution
-  Serial.print(pulselength); Serial.println(" us per bit"); 
-  pulse *= 1000000;  // convert input seconds to us
-  pulse /= pulselength;
-  Serial.println(pulse);
-  pwm.setPWM(n, 0, pulse);
-}
-
 void loop() {
-  //if (BackBumper == 0)
+
+  if (BackBumper == 0)
+  {
+    BackFlg = 1;
+  }
+  
+  //if (BackFlg == 1)
   //{
+  if(startFlg == 0)
+  {
+  for (uint8_t i = 0; i < 15; i++)
+  {
+      for (uint16_t pulselen = 50; pulselen < 200; pulselen+=5) {
+        pwm.setPWM(i, 0, pulselen);
+      }
+  }
+  startFlg = 1;
+  }
     // Drive each servo one at a time using setPWM()
     Serial.println(servonum[Counter]);
-    for (uint16_t pulselen = SERVOMIN; pulselen < SERVOMAX; pulselen++) {
+    for (uint16_t pulselen = SERVOMIN; pulselen < SERVOMAX; pulselen+=5) {
       pwm.setPWM(servonum[Counter], 0, pulselen);
     }
 
     delay(500);
-    for (uint16_t pulselen = SERVOMAX; pulselen > SERVOMIN; pulselen--) {
+    for (uint16_t pulselen = SERVOMAX; pulselen > SERVOMIN; pulselen-=5) {
       pwm.setPWM(servonum[Counter], 0, pulselen);
     }
 
     delay(500);
-
+    
     Counter++;
     if (Counter == 27)
       while(1);//spin loop
   //}
 }
-
