bdf252d59287df19b323deaa30bc058ac2223e1d
Ajen8005 <45045045+Ajen8005@users.noreply.github.com>
2020-01-26 20:34:00 -0500

    Update SmallBot.ino

--- SmallBot.ino
+++ SmallBot.ino
@@ -9,13 +9,11 @@
 
 #include <Wire.h>
 #include <Adafruit_PWMServoDriver.h>
+#include <SD.h>
+#include <SPI.h> 
+#include "LowPower.h"
 
 Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
-
-// Depending on your servo make, the pulse width min and max may vary, you 
-// want these to be as small/large as possible without hitting the hard stop
-// for max range. You'll have to tweak them as necessary to match the servos you
-// have!
 //350 - 180 degree
 //SERVOMIN 50 is 45 degrees
 // Every 50 is 22.5 degrees for servo max when servo min is 50
@@ -25,58 +23,104 @@ Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
 #define SERVO_FREQ 50 // Analog servos run at ~50 Hz updates
 
 // our servo # counter
-uint8_t servonum[100] = {3,1,4,1,5,9,2,6,5,3,5,8,9,7,9,3,2,3,8,4,6,2,6,4,3,3,8,3};
+uint8_t servonum;
 uint8_t Counter = 0;
 uint8_t FrontBumper, BackBumper, BackFlg = 0, Front = 0, startFlg = 0;
+const int wakeUpPin = 2;
 
 void setup() {
+  File myFile;
   Serial.begin(9600);
   Serial.println("8 channel Servo test!");
 
+  //pinMode(wakeUpPin, INPUT)
+  
   pwm.begin();
-  // In theory the internal oscillator is 25MHz but it really isn't
-  // that precise. You can 'calibrate' by tweaking this number till
-  // you get the frequency you're expecting!
   pwm.setOscillatorFrequency(27000000);  // The int.osc. is closer to 27MHz  
   pwm.setPWMFreq(SERVO_FREQ);  // Analog servos run at ~50 Hz updates
 
-  delay(10);
-}
-
-void loop() {
-
-  if (BackBumper == 0)
-  {
-    BackFlg = 1;
+  if (!SD.begin()){
+    Serial.println("initialization failed!");
+    while(1);
   }
+
+  myFile = SD.open("pi.txt", FILE_READ);
+  if (myFile){
+    serial.println("pi.txt:");
+  }
+  //read from the file until there's nothing else in it:
+  servonum = myFile.read();
   
-  //if (BackFlg == 1)
-  //{
-  if(startFlg == 0)
-  {
-  for (uint8_t i = 0; i < 15; i++)
-  {
-      for (uint16_t pulselen = 50; pulselen < 200; pulselen+=5) {
-        pwm.setPWM(i, 0, pulselen);
-      }
-  }
-  startFlg = 1;
-  }
-    // Drive each servo one at a time using setPWM()
-    Serial.println(servonum[Counter]);
-    for (uint16_t pulselen = SERVOMIN; pulselen < SERVOMAX; pulselen+=5) {
-      pwm.setPWM(servonum[Counter], 0, pulselen);
-    }
-
-    delay(500);
-    for (uint16_t pulselen = SERVOMAX; pulselen > SERVOMIN; pulselen-=5) {
-      pwm.setPWM(servonum[Counter], 0, pulselen);
-    }
-
-    delay(500);
-    
-    Counter++;
-    if (Counter == 27)
-      while(1);//spin loop
-  //}
 }
+
+void loop() 
+{ 
+  //Interrupt and sleep code:::
+  //attachInterrupt( 0, pushPi, LOW)
+  //LowPower.powerDown(SLEEP_FOREVER, ADC_OFF, BOD_OFF);
+      if(startFlg == 0)
+      {
+        for (uint8_t i = 0; i < 15; i++)
+        {
+          for (uint16_t pulselen = 50; pulselen < 200; pulselen+=5) {
+          pwm.setPWM(i, 0, pulselen);
+        }
+      }
+        startFlg = 1;
+      }
+      
+      // Drive each servo one at a time using setPWM()
+      Serial.println(servonum);
+      for (uint16_t pulselen = SERVOMIN; pulselen < SERVOMAX; pulselen+=5) {
+        pwm.setPWM(servonum, 0, pulselen);
+      }
+
+      delay(500);
+      for (uint16_t pulselen = SERVOMAX; pulselen > SERVOMIN; pulselen-=5) {
+        pwm.setPWM(servonum, 0, pulselen);
+      }
+
+      delay(500);
+      Counter++;
+      servonum = myFile.read();
+}
+
+//void pushPi()
+//{
+//  LowPower.idle(SLEEP_FOREVER, ADC_OFF, TIMER2_ON, TIMER1_ON, TIMER0_ON, SPI_ON, USART0_OFF, TWI_ON);
+//  while(1)
+//  {
+//        if(startFlg == 0)
+//      {
+//        for (uint8_t i = 0; i < 15; i++)
+//        {
+//          for (uint16_t pulselen = 50; pulselen < 200; pulselen+=5) {
+//          pwm.setPWM(i, 0, pulselen);
+//        }
+//      }
+//        startFlg = 1;
+//      }
+//      
+//      // Drive each servo one at a time using setPWM()
+//      Serial.println(servonum[Counter]);
+//      for (uint16_t pulselen = SERVOMIN; pulselen < SERVOMAX; pulselen+=5) {
+//        pwm.setPWM(servonum[Counter], 0, pulselen);
+//      }
+//
+//      delay(500);
+//      for (uint16_t pulselen = SERVOMAX; pulselen > SERVOMIN; pulselen-=5) {
+//        pwm.setPWM(servonum[Counter], 0, pulselen);
+//      }
+//
+//      delay(500);
+//      Counter++;
+//      if (Counter == 3)
+//        Counter = 0;
+//      servonum[Counter] = myFile.read();
+//  }
+//}
+
+//Low Power example and Library
+//https://github.com/rocketscream/Low-Power
+//It looks like a new function is going to be created and theres is going ot be an interrupt pin on
+//the back bumper to wake it up then it will stay awake.
